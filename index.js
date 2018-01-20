4362356981cacda6e189007fc89ca4af9a758eaf
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-20 21:23:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeli = []
 function takeANumber(katzDeliLine, name) {
+  katzDeliLine += 1
   var greeting = `Welcome, ${name}. You are number ${katzDeliLine} in line.`
   return greeting
 }
