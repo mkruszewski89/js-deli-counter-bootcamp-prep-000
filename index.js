a68ef0764104ac6166c46d9cb0219ded06456d76
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-20 22:11:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var katzDeliLine = []
-
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   var positionInLine = katzDeliLine.length
@@ -16,4 +14,18 @@ function nowServing(katzDeliLine) {
     whoseTurn = "There is nobody waiting to be served!"
   }
   return whoseTurn
+}
+
+function currentLine(katzDeliLine) {
+  var line
+  if (katzDeliLine.length > 0) {
+    line = `The line is currently:`
+    for (var i = 0; i < katzDeliLine.length; i += 1) {
+      line = line + ` ${i + 1}. ${katzDeliLine[i]},`
+    }
+    line = line.substring(0, line.length -1)
+  } else {
+    line = `The line is currently empty.`
+  }
+  return line
 }
